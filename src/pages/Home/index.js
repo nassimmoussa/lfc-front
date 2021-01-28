25d09d064e02a3444236fd7cc9b2aeb04e6f2a36
Nassim Moussa <nassimmoussa1993@gmail.com>
2021-01-27 22:17:38 -0300

    create room basic structure

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+import Loading from 'components/Loading';
+import Button from '@material-ui/core/Button';
+
+import { useStyles } from './styles';
+
+const WS_ENDPOINT = process.env.REACT_APP_WS_URL;
 
 const Home = () => {
+  const classes = useStyles();
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = socketIOClient(WS_ENDPOINT);
+
+    setSocket(newSocket);
+  }, []);
+
+  if (!socket) {
+    return <Loading />;
+  }
+
+  const createRoomHandler = () => {
+    socket.emit('createRoom', { students: ['aluno1', 'aluno2'] });
+  };
+
+  socket.on('roomCreated', ({ roomId }) => {
+    console.log(roomId);
+  });
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className={classes.createRoomContainer}>
+      <Button
+        variant="outlined"
+        size="large"
+        color="primary"
+        onClick={createRoomHandler}
+      >
+        CRIAR SALA VIRTUAL
+      </Button>
     </div>
   );
 };
